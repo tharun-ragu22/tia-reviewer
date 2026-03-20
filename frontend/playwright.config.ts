49307a9988f7c1eb8da43dca13ce74ea03bb5866
tharun-ragu22 <tharunragulan@gmail.com>
2026-03-20 19:00:12 -0400

    run frontend acceptance tests

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -1,4 +1,13 @@
 import { defineConfig } from '@playwright/test'
+import os from 'os'
+import path from 'path'
+import fs from 'fs'
+
+if (process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON) {
+  const tmpPath = path.join(os.tmpdir(), 'gcp-key.json')
+  fs.writeFileSync(tmpPath, process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON)
+  process.env.GOOGLE_APPLICATION_CREDENTIALS = tmpPath
+}
 
 export default defineConfig({
   testDir: './__e2e__',
