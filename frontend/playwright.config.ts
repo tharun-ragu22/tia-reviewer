57eb4847ee7647c9d06c8819c3c381b71b31e85f
tharun-ragu22 <tharunragulan@gmail.com>
2026-03-20 19:53:00 -0400

    add env to playwright config for webserver to make storage call

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -1,22 +1,28 @@
-import { defineConfig } from '@playwright/test'
-import os from 'os'
-import path from 'path'
-import fs from 'fs'
+import { defineConfig } from "@playwright/test";
+import os from "os";
+import path from "path";
+import fs from "fs";
 
 if (process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON) {
-  const tmpPath = path.join(os.tmpdir(), 'gcp-key.json')
-  fs.writeFileSync(tmpPath, process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON)
-  process.env.GOOGLE_APPLICATION_CREDENTIALS = tmpPath
+  const tmpPath = path.join(os.tmpdir(), "gcp-key.json");
+  fs.writeFileSync(tmpPath, process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON);
+  process.env.GOOGLE_APPLICATION_CREDENTIALS = tmpPath;
 }
 
 export default defineConfig({
-  testDir: './__e2e__',
+  testDir: "./__e2e__",
   use: {
-    baseURL: 'http://localhost:3000',
+    baseURL: "http://localhost:3000",
   },
   webServer: {
-    command: 'npm run dev',
-    url: 'http://localhost:3000',
+    command: "npm run dev",
+    url: "http://localhost:3000",
     reuseExistingServer: !process.env.CI,
+    env: {
+      GCP_PROJECT_ID: process.env.GCP_PROJECT_ID ?? "",
+      GCS_BUCKET_NAME: process.env.GCS_BUCKET_NAME ?? "",
+      GOOGLE_APPLICATION_CREDENTIALS:
+        process.env.GOOGLE_APPLICATION_CREDENTIALS ?? "",
+    },
   },
-})
+});
